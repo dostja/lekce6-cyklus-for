cc09a12ab068530336fda7019b269a31e372aaf2
dostja <dostalova2jana@seznam.cz>
2021-04-16 20:52:53 +0200

    For

--- main.js
+++ main.js
@@ -1,31 +1,24 @@
 // Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
-
 // První příklad - vypiš vzestupně čísla od 0 do 10
-console.log('První příklad - vzestupná řada 0 - 10');
-
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
-
-
+console.log("První příklad - čísla 0 až 10");
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
 
-console.log('Druhý příklad - rozdíl mezi <= a <');
-
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
-
-
+console.log("Druhý příklad - rozdíl mezi <= a <");
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
-console.log('Třetí příklad - sestupná řada 10 - 0');
+console.log("Třetí příklad - sestupná řada 10 - 0");
+for (let i=10; i>= 0; i--){
+    console.log(i);
+}
 
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
