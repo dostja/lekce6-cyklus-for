290b84cf42aa74e841a003f51e3c6aa6aaf3cc70
dostja <dostalova2jana@seznam.cz>
2021-04-16 21:24:30 +0200

    While

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-// Použij výpis do konzole prohlížeče pomocí console.log('text')
-// Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
-// První příklad - vypiš vzestupně čísla od 0 do 10
-console.log("První příklad - čísla 0 až 10");
+// FOR CYKLUS
+console.log("For první příklad - čísla 0 až 10");
 for (let i = 0; i <= 10; i++) {
   console.log(i);
 }
 
-// Druhý příklad - zkopíruj předchozí příklad
-// a vyzkoušej, jaký je rozdíl mezi <= a <
-// v podmínce uvnitř cyklu
-
-console.log("Druhý příklad - rozdíl mezi <= a <");
+console.log("For druhý příklad - rozdíl mezi <= a <");
 for (let i = 0; i < 10; i++) {
   console.log(i);
 }
 
-// Třetí příklad - vypiš sestupně čísla od 10 do 0
-console.log("Třetí příklad - sestupná řada 10 - 0");
+
+console.log("For třetí příklad - sestupná řada 10 - 0");
 for (let i=10; i>= 0; i--){
     console.log(i);
 }
 
+//WHILE CYKLUS
+
+console.log(" While první příklad - čísla 0 až 10");
+let a=-1;
+while(a<10) {
+  a++;
+  console.log(a);
+}
+
+console.log(" While druhý příklad - rozdíl mezi <= a <");
+let b=-1;
+while(b<=10) {
+  b++;
+  console.log(b);
+}
+
+console.log(" While třetí příklad - 10 až 0");
+let c=11;
+while(c>0) {
+  c--;
+  console.log(c);
+}
